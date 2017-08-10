5e3b626b445bf4d52c41ef1638f09def5f412c52
Igor Matheus de Souza <igorm1411@gmail.com>
2017-08-09 23:08:22 -0300

    Add files via upload

---
+++ jogoDeCartas/jogo2.aspx
@@ -0,0 +1,291 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="jogo.aspx.cs" Inherits="jogoDeCartas.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <link type="text/css" rel="stylesheet" href="frameworks/bootstrap.min.css" />
+    <script src="frameworks/jquery-3.2.1.min.js"></script>
+    <title>Jogo de Poker</title>
+<style>
+#Background{
+    
+    width: 100%;
+    height: auto;
+    
+    bottom: 0;
+    right: 0;
+    min-width: 100%;
+    min-height: 100%;
+    position:fixed;
+    z-index: -1000;
+
+    
+    }
+
+    .cartas {
+        width: 120px;
+        height: 180px;
+        background-image: url("imgs/backcardRed.png");
+        margin: 5px 5px 5px 5px;
+        display: inline-block;
+        background-size: cover;
+    }
+    #jogador1, #jogador2 {
+    width:670px;
+    margin:auto;
+    display:block;
+    }
+    hr {
+    margin:80px 0 80px 0;
+    }
+    .cartas img {
+    border-radius:10px;
+    }
+
+    #receberdados , .imgCartas{
+        display:none;
+    }
+
+    #jogador2 >.cartas {
+    background-image: url("imgs/backcardBlue.png")
+    }
+
+    .myBtn {
+       display:block;
+       width:200px;
+       margin:40px auto -40px auto ;
+    
+       font-size:20px;
+    }
+
+    .textoVencedor {
+     display:none;
+    margin-top:-20px;
+    font-size:25px;
+    
+        
+         }
+
+    .text-white {
+    color:white;
+    }
+
+    footer {
+    margin-top:50px;
+    }
+
+    #logo-footer {
+    margin:auto;
+    display:block;
+    }
+</style>
+
+    
+</head>
+<body  runat="server">
+
+    <img id="Background" src="imgs/JingleBells_game_background.jpg" alt="backg"/>
+
+
+    <div id="jogador1">
+        <h2 class="text-center text-danger">Jogador 1</h2>
+        <div class="cartas">
+        <asp:Image runat="server" class="imgCartas"  Id="image1"/>
+        </div>
+        <div class="cartas">
+        <asp:Image runat="server" class="imgCartas"  Id="image2" />
+        </div>
+        <div class="cartas">
+        <asp:Image runat="server" class="imgCartas" Id="image3" />
+         </div>
+        <div class="cartas">
+        <asp:Image runat="server" class="imgCartas" Id="image4" />
+         </div>
+        <div class="cartas">
+        <asp:Image runat="server" class="imgCartas" Id="image5" /> 
+          </div>
+
+    </div>
+
+
+    <div class="btn btn-success myBtn" onclick="definirVencedor()">Virar as cartas</div>
+
+    <hr/>
+    <div class="textoVencedor text-center">55</div>
+
+    <div id="jogador2">
+
+        <h2 class="text-center text-info">Jogador 2</h2>
+        <div class="cartas">
+        <asp:Image runat="server" Id="image6" class="imgCartas" />
+        </div>
+        <div class="cartas">
+        <asp:Image runat="server" Id="image7" class="imgCartas" />
+        </div>
+        <div class="cartas">
+        <asp:Image runat="server" Id="image8" class="imgCartas" />
+         </div>
+        <div class="cartas">
+        <asp:Image runat="server" Id="image9" Class="imgCartas" />
+         </div>
+        <div class="cartas">
+        <asp:Image runat="server" Id="image10" class="imgCartas"/>
+          </div>
+
+    </div>
+    
+   
+
+    
+
+    
+
+    <div id="receberdados">    
+            <asp:label ID="Label1" runat="server"  >Hi</asp:label>
+            <asp:label ID="Label2" runat="server"  >jogador 1</asp:label>
+            <asp:label ID="Label3" runat="server"  >Jogador 2</asp:label>
+            <asp:Label ID="Label4" runat="server" ></asp:Label>
+            <asp:Label ID="Label5" runat="server" ></asp:Label>
+
+   </div>       
+
+    
+<footer>
+<h4 class="text-center text-white">Developed by Igor Matheus de Souza for ASC Solutions</h4>
+    <img src="imgs/logo.png" id="logo-footer"/>
+
+
+</footer>
+
+    
+
+    <script type="text/javascript">
+
+        //Pegar informações do codebehind
+        var posicaoDasCartas = document.getElementById("Label1").innerHTML;
+        
+        posicaoDasCartas = posicaoDasCartas.split("~");
+        var Jogador1cartas= document.getElementById("Label2").innerHTML;
+        var Jogador2cartas= document.getElementById("Label3").innerHTML;
+
+
+        //colocar todas as cartas na pagina
+        var classeDasCartas = document.getElementsByClassName("imgCartas");
+        
+        
+       
+        for (var i = 0; i < 10; i++) {
+            
+            classeDasCartas[i].src = "imgs/" + posicaoDasCartas[i]+ ".png";
+ 
+        }
+       
+        //clicar no botao
+        function definirVencedor() {
+
+            if ($(".myBtn").text() == "Jogar novamente") {
+                location.reload();
+                return;
+            }
+
+
+            var txt = $(".textoVencedor");
+
+            $(".imgCartas").fadeIn(1200);
+
+            $(".myBtn").text("Jogar novamente");
+
+
+
+            var rankjogador =["",""];
+            
+            //ver qual foi o rank das cartas na mao dos jogadores
+
+            for (var i = 0; i < 2; i++) {
+                var jotual;
+                if (i == 0){joAtual =parseInt( Jogador1cartas) };
+                if (i == 1) { joAtual = parseInt(Jogador2cartas) };
+
+                
+                switch (joAtual) {
+                    case 1:
+                        rankjogador[i] ="High card";
+                        break;
+                    case 2:
+                        rankjogador[i] ="One pair";
+                        break;
+                    case 3:
+                        rankjogador[i] = "Two pair";
+                        break;
+                    case 4:
+                        rankjogador[i] = "Three of a kind";
+                        break;
+                    case 5:
+                        rankjogador[i] = "Straight";
+                        break;
+                    case 6:
+                        rankjogador[i] = "Flush";
+                        break;
+                    case 7:
+                        rankjogador[i] = "Full House";
+                        break;
+                    case 8:
+                        rankjogador[i] = "Four of a Kind";
+                        break;
+                    case 9:
+                        rankjogador[i] = "Straight Flush";
+                        break;
+                    case 10:
+                        rankjogador[i] = "Royal Flush";
+                        break;
+                    default:
+                        rankjogador[i] = "5555";
+        
+                        
+                }
+
+                
+            }
+
+            //definir o texto do ganhador
+            if (Jogador1cartas > Jogador2cartas) {
+                txt.text("Jogador 1 ganhou com um "+ rankjogador[0]+" X " +rankjogador[1]);
+                txt.addClass("text-danger");
+            
+            }
+            if (Jogador1cartas < Jogador2cartas) {
+                txt.text("Jogador 2 ganhou com um " + rankjogador[1] + " X " + rankjogador[0]);
+                txt.addClass("text-info");
+
+            }
+            if (Jogador1cartas == Jogador2cartas) {
+                var desempatej1 = parseInt(document.getElementById("Label4").innerHTML);
+                var desempatej2 = parseInt(document.getElementById("Label5").innerHTML);
+                
+
+                if (desempatej1 > desempatej2) {
+
+                    txt.text("Impatou com um " + rankjogador[0] + " X " + rankjogador[1] + ", mas o Jogador 1 ganhou por ter a carta maior");
+                    txt.addClass("text-danger");
+                }
+                else if (desempatej1 < desempatej2) {
+
+                    txt.text("Impatou com um " + rankjogador[0] + " X " + rankjogador[1] + ", mas o Jogador 2 ganhou por ter a carta maior");
+                    txt.addClass("text-info");
+                }
+                else {
+                    txt.text("impatou com " + rankjogador[1] + " X " + rankjogador[0]);
+                    txt.addClass("text-white");
+                }
+
+           }
+        
+            
+            
+            txt.fadeIn(2000);
+        }
+
+    </script>
+</body>
+</html>
---
+++ jogoDeCartas/jogo2.aspx.cs
@@ -0,0 +1,350 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Text;
+
+
+namespace jogoDeCartas
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            String Naipes="" ;
+
+            String[] naipesdojogador1 = new String[5];
+            int[] cartasDOsJogador1 = new int[5];
+            String[] naipesdojogador2 = new String[5];
+            int[] cartasDOsJogador2 = new int[5];
+
+            String ParaJS = "";
+            
+            // gerar naipes e cartas aleatorias para o jogador 1
+
+            for (int i = 0; i < 5; i++)
+            {
+                Boolean verificar = true; 
+                int numeroAlea= numeroAleatorio().Next(1,5);
+
+                if( numeroAlea==1 )   { Naipes="c";}
+                if( numeroAlea==2 )   { Naipes="e";}
+                if( numeroAlea==3 )   { Naipes="o";}
+                if( numeroAlea==4 )   { Naipes="p";}
+
+                naipesdojogador1[i] = Naipes;
+
+                numeroAlea = numeroAleatorio().Next(2, 15);
+
+                cartasDOsJogador1[i] = numeroAlea;
+
+                        //verificar se as cartas se repetem.
+                for (int k = 0; k < i; k++)
+                {
+                    if( naipesdojogador1[i]==naipesdojogador1[k] && cartasDOsJogador1[i] ==  cartasDOsJogador1[k])
+                    {
+                    --i;
+                    verificar = false;
+                    break;
+                    }
+                }
+
+
+             if(verificar)
+                ParaJS += naipesdojogador1[i] + cartasDOsJogador1[i] + "~";
+            }
+
+
+
+            // gerar naipes e cartas aleatorias para o jogador 2
+
+            for (int i = 0; i < 5; i++)
+            {
+                Boolean verificar = true; 
+                int numeroAlea = numeroAleatorio().Next(1, 5);
+
+                if (numeroAlea == 1) { Naipes = "c"; }
+                if (numeroAlea == 2) { Naipes = "e"; }
+                if (numeroAlea == 3) { Naipes = "o"; }
+                if (numeroAlea == 4) { Naipes = "p"; }
+
+                naipesdojogador2[i] = Naipes;
+
+                numeroAlea = numeroAleatorio().Next(2, 15);
+
+                cartasDOsJogador2[i] = numeroAlea;
+
+                            //verificar se as cartas se repetem.
+                for (int k = 0; k < i; k++)
+                {
+                    if (naipesdojogador2[i] == naipesdojogador2[k] && cartasDOsJogador2[i] == cartasDOsJogador2[k])
+                    {
+                        i--;
+                        verificar = false;
+                        break;
+                    }
+                }
+
+                for (int k = 0; k < 5; k++)
+                {
+
+                    if (naipesdojogador2[i] == naipesdojogador1[k] && cartasDOsJogador2[i] == cartasDOsJogador1[k])
+                    {
+                        i--;
+                        verificar = false;
+                        break;
+                    }
+                }
+
+
+                if (verificar)
+                 ParaJS += naipesdojogador2[i] + cartasDOsJogador2[i] + "~";
+           }
+
+
+            Array.Sort(cartasDOsJogador1);
+            Array.Sort(cartasDOsJogador2);
+
+            // chamar a função para obter o resultado
+            int maoDoJogador1 = verificarResultado(cartasDOsJogador1,naipesdojogador1);
+            int maoDoJogador2 = verificarResultado(cartasDOsJogador2, naipesdojogador2);
+
+            Label2.Text = "" + maoDoJogador1;
+            Label3.Text = "" + maoDoJogador2;
+
+            // verificar qual mão possui as maiores cartas caso empate
+            if (maoDoJogador1 == 1 && maoDoJogador2 == 1)
+            {
+                Array.Sort(cartasDOsJogador1);
+                Array.Sort(cartasDOsJogador2);
+
+                for (int i = 4; i >= 0; i--)
+                {
+
+
+                    if (cartasDOsJogador1[i] != cartasDOsJogador2[i])
+                    {
+                        Label4.Text = "" + cartasDOsJogador1[i];
+                        Label5.Text = "" + cartasDOsJogador2[i];
+                        break;
+
+                    }
+
+
+                };
+            }
+
+
+            // enviar para javascript
+            Label1.Text = ParaJS;
+
+
+
+        }
+
+        public static int verificarResultado( int[] cartasDeAlgumJogador, String[] naipesDeAlgumjogador){
+
+            int resultado = 0;
+
+            int contRoyal = 0;
+            int contStraightFl = 0;
+            int contFullHouse = 0;
+            int contFlush = 0;
+            int contStraight = 0;
+            int contThreeOfaKind = 0;
+            int contOnePair = 0;
+            
+            for (int i = 0; i < 5; i++)
+            {
+
+
+                // Royal Flush jogador 1
+                if ( cartasDeAlgumJogador[i] > 9)
+                {
+                    contRoyal++;
+                }
+                if (i < 4)
+                {
+
+                    if (naipesDeAlgumjogador[i] == naipesDeAlgumjogador[i + 1])
+                    {
+                        contRoyal++;
+                    }
+                }
+                if (i == 4 && contRoyal == 9)
+                {
+                    resultado = 10;
+                    
+                    return resultado;
+
+                }
+
+                // StraightFlush jogador 1
+               
+
+
+                if (i < 4)
+                {
+
+                    if (naipesDeAlgumjogador[i] == naipesDeAlgumjogador[i + 1])
+                    {
+                        contStraightFl++;
+                    }
+                    if ( cartasDeAlgumJogador[i] <  cartasDeAlgumJogador[i + 1])
+                    {
+                        contStraightFl++;
+                    }
+                }
+
+                if (i == 4 && contStraightFl == 8)
+                {
+                    resultado = 9;
+
+                    return resultado;
+
+                }
+
+
+                // four of kind
+
+                if (i == 4 && ( cartasDeAlgumJogador[0] ==  cartasDeAlgumJogador[1] &&  cartasDeAlgumJogador[2] ==  cartasDeAlgumJogador[1]
+                    &&  cartasDeAlgumJogador[2] ==  cartasDeAlgumJogador[3]) || ( cartasDeAlgumJogador[1] ==  cartasDeAlgumJogador[2]
+                     &&  cartasDeAlgumJogador[4] ==  cartasDeAlgumJogador[3] &&  cartasDeAlgumJogador[2] ==  cartasDeAlgumJogador[3]))
+                {
+
+                    resultado = 8;
+
+                    return resultado;
+                }
+
+                //full house
+
+                if (i < 4)
+                {
+                    if ( cartasDeAlgumJogador[i] ==  cartasDeAlgumJogador[i + 1])
+                    {
+                        contFullHouse++;
+                    }
+                }
+                if (i == 4 && contFullHouse == 3)
+                {
+
+                    resultado = 7;
+
+                    return resultado;
+                }
+
+
+
+                //flush
+                if (i < 4)
+                {
+
+                    if (naipesDeAlgumjogador[i] == naipesDeAlgumjogador[i + 1])
+                    {
+                        contFlush++;
+                    }
+                }
+
+                if (i == 4 && contFlush == 4)
+                {
+
+                    resultado = 6;
+
+                    return resultado;
+                }
+
+
+
+                //Straight
+                if (i < 4)
+                {
+                    if ( cartasDeAlgumJogador[i] + 1 ==  cartasDeAlgumJogador[i + 1])
+                    {
+                        contStraight++;
+                    }
+                }
+                if (i == 4 && contStraight == 4)
+                {
+                    resultado = 5;
+
+                    return resultado;
+
+                }
+
+                //Three of a kind
+                if (i < 3)
+                {
+                    if ( cartasDeAlgumJogador[i] ==  cartasDeAlgumJogador[i + 1] &&  cartasDeAlgumJogador[i] ==  cartasDeAlgumJogador[i + 2])
+                    {
+                        contThreeOfaKind++;
+                    }
+
+                }
+                if (i == 4 && contThreeOfaKind == 1)
+                {
+                    resultado = 4;
+
+                    return resultado;
+
+                }
+
+                //two pair
+                if (i == 4 && (( cartasDeAlgumJogador[0] ==  cartasDeAlgumJogador[1] &&  cartasDeAlgumJogador[2] ==  cartasDeAlgumJogador[3]) ||
+                    ( cartasDeAlgumJogador[1] ==  cartasDeAlgumJogador[2] &&  cartasDeAlgumJogador[4] ==  cartasDeAlgumJogador[3]) ||
+                    ( cartasDeAlgumJogador[0] ==  cartasDeAlgumJogador[1] &&  cartasDeAlgumJogador[3] ==  cartasDeAlgumJogador[4])))
+                {
+                    resultado = 3;
+
+                    return resultado;
+
+
+                }
+                //one pair
+                if (i < 4)
+                {
+                    if ( cartasDeAlgumJogador[i] ==  cartasDeAlgumJogador[i + 1])
+                    {
+                        contOnePair++;
+                    }
+                }
+                if (i == 4 && contOnePair == 1)
+                {
+                    resultado = 2;
+
+                    return resultado;
+                }
+
+                //High Card
+
+                if (i == 4)
+                {
+                    resultado = 1;
+
+                    return resultado;
+
+                }
+
+               
+            }//Fim do for 
+
+            return resultado;
+        }
+        
+
+        public static Random numeroAleatorio()
+        {
+            Random r = new Random();
+            for (int k = 0; k < 5255; k++)
+            {
+                r = new Random();
+            }
+            return r;
+        }
+    }
+
+    
+
+}
